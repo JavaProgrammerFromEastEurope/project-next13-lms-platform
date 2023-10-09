6a1335580b5e04511e39f64b51492435d15a0ba3
Vladimir <gordonfree.com@gmail.com>
2023-10-09 23:47:46 +0300

    update prisma

--- app/api/courses/[courseId]/route.ts
+++ app/api/courses/[courseId]/route.ts
@@ -1,10 +1,64 @@
+import Mux from "@mux/mux-node";
 import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 import { db } from "@/lib/db";
 
+const { Video } = new Mux(
+	process.env.MUX_TOKEN_ID!,
+	process.env.MUX_TOKEN_SECRET!,
+);
+
+export async function DELETE(
+	req: Request,
+	{ params }: { params: { courseId: string } }
+) {
+	try {
+		const { userId } = auth();
+
+		if (!userId) {
+			return new NextResponse("Unauthorized", { status: 401 });
+		}
+
+		const course = await db.course.findUnique({
+			where: {
+				id: params.courseId,
+				userId: userId,
+			},
+			include: {
+				chapters: {
+					include: {
+						muxData: true,
+					}
+				}
+			}
+		});
+
+		if (!course) {
+			return new NextResponse("Not found", { status: 404 });
+		}
+
+		for (const chapter of course.chapters) {
+			if (chapter.muxData?.assetId) {
+				await Video.Assets.del(chapter.muxData.assetId);
+			}
+		}
+
+		const deleteCourse = await db.course.delete({
+			where: {
+				id: params.courseId,
+			}
+		});
+
+		return NextResponse.json(deleteCourse);
+	} catch (error) {
+		console.log("[COURSE_ID_DELETE]", error);
+		return new NextResponse("internal Error", { status: 500 });
+	}
+}
+
 export async function PATCH(
 	req: Request,
-	{ params }: { params: { courseId: string }}
+	{ params }: { params: { courseId: string } }
 ) {
 	try {
 		const { userId } = auth();
@@ -17,7 +71,7 @@ export async function PATCH(
 
 		const course = await db.course.update({
 			where: {
-				id: params.courseId,
+				id: courseId,
 				userId
 			},
 			data: {
@@ -28,6 +82,6 @@ export async function PATCH(
 		return NextResponse.json(course);
 	} catch (error) {
 		console.log("[COURSE_ID]", error);
-		return new NextResponse("Internal Error", {status: 500})
+		return new NextResponse("Internal Error", { status: 500 })
 	}
 };
